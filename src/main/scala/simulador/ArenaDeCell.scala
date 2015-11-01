90b06c17ae1648a727ed7140d16a253afcd769b2
Ramiro Vazquez <ramirov93@gmail.com>
2015-10-31 23:32:18 -0300

    Hice el dejarseFajar (por ahora así) y el cargarKi. Fui por el camino de las tuplas que habías planteado, el de los movimientos como funciones que las manejan y los Guerreros como cosas inmutables que tienen una Raza, la cual es pattern matcheable y cada una tiene sus propios valores. También agregué unos test del cargarKi.

--- src/main/scala/simulador/ArenaDeCell.scala
+++ src/main/scala/simulador/ArenaDeCell.scala
@@ -7,24 +7,38 @@ import scala.collection.GenTraversableOnce
 object ArenaDeCell {
   
   case class Guerrero(raza: Raza, ki: Int = 0, items: List[Item] = List(), movimientos: List[Movimiento] = List()) {
-
-  def aumentarKi(cuanto: Int) = copy(ki = ki + cuanto)
-  def disminuirKi(cuanto: Int) = copy(ki = ki - cuanto)
-  def learnMovement(unMovimiento: Movimiento) = copy(movimientos = movimientos.+:(unMovimiento))
-  def agregarItem(unItem: Item) = copy(items = items.+:(unItem))
-  def removerItem(unItem: Item) = copy(items = items.filterNot { item => item == unItem })//este metodo lo hace, el que lo llama tiene que ser consciente de que tenga el item
+    
+    def aumentarKi(cuanto: Int) = copy(ki = ki + cuanto)
+    def disminuirKi(cuanto: Int) = copy(ki = ki - cuanto)
+    def learnMovement(unMovimiento: Movimiento) = copy(movimientos = movimientos.+:(unMovimiento))
+    def agregarItem(unItem: Item) = copy(items = items.+:(unItem))
+    def removerItem(unItem: Item) = copy(items = items.filterNot { item => item == unItem })//este metodo lo hace, el que lo llama tiene que ser consciente de que tenga el item
 }
   
   trait Item {}
   
-  trait Raza {}
-  case class Monster() extends Raza {}
-  case class Human() extends Raza {}
-  case class Android() extends Raza {}
-  case class Namekusein() extends Raza {}
-  case class Saiyajin() extends Raza {}
+  abstract class Raza
   
-  type Movimiento = Function1[Guerrero, Option[Guerrero]]
-  case class DejarseFarjar() extends Movimiento{}
+  case class Monstruo() extends Raza {}
+  case class Humano() extends Raza {}
+  case class Androide(bateria: Int) extends Raza {}
+  case class Namekusein() extends Raza {}
+  case class Saiyajin(cola: Boolean, nivel: Int = 0) extends Raza {}
+
+  type Movimiento = Luchadores => Luchadores
+  
+  type Luchadores = (Guerrero, Option[Guerrero])
+  
+  val dejarseFajar = (luchadores: Luchadores) => luchadores
+  
+  val cargarKi = (luchadores: Luchadores) => {
+    luchadores._1.raza match {
+      case Androide(_) => luchadores
+      case Saiyajin(cola, nivel) => (luchadores._1.aumentarKi(150 * nivel), luchadores._2)
+      case _ => (luchadores._1.aumentarKi(100), luchadores._2)
+    }
+  }
+  
+  
   
 }
---
+++ src/test/scala/TestMovimientos.scala
@@ -0,0 +1,27 @@
+
+import simulador.ArenaDeCell._
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class TestMovimientos extends FlatSpec with Matchers {
+  
+  val mrSatan = Guerrero(Humano(), 1000, List(), List())
+  val goku = Guerrero(Saiyajin(true, 3), 20000, List(), List())
+  val androide18 = Guerrero(Androide(1000), 0, List(), List())
+  
+  "mrSatan" should "cargarKi y subir 100 de ki por ser Guerrero" in {
+
+    assert(cargarKi(mrSatan, null)._1.ki === 1100)
+  }
+
+  "goku" should "cargarKi y subir ki en  150 * nivel de SS por ser Saiyajin" in {
+
+    assert(cargarKi(goku, null)._1.ki === 20450)
+  }
+ 
+  "androide18" should "cargarKi y no subir nada (seguir en 0) por ser un Androide" in {
+
+    assert(cargarKi(androide18, null)._1.ki === 0)
+  }
+  
+}
