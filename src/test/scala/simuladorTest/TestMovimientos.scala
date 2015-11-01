f2a60f2ac1dbe02096c9c544ad58f7798c41b816
JuanchiRios <juanchirios04@hotmail.com>
2015-11-01 01:41:32 -0300

    refactor para que los items sean sets

--- src/test/scala/simuladorTest/TestMovimientos.scala
+++ src/test/scala/simuladorTest/TestMovimientos.scala
@@ -2,13 +2,17 @@
 import simulador.ArenaDeCell._
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
+import scala.collection.immutable.List
+import scala.collection.immutable.Set
+import scala.collection.GenTraversableOnce
+
 
 class TestMovimientos extends FlatSpec with Matchers {
   
-  val mrSatan = Guerrero(Humano(), 1000, List(), List(), Normal)
-  val bulma = Guerrero(Humano(), 100, List(), List(), Normal)
-  val goku = Guerrero(Saiyajin(true, 3), 20000, List(), List(), Normal)
-  val androide18 = Guerrero(Androide(1000), 0, List(), List(), Normal)
+  val bulma = Guerrero(Humano(), 100, List(), Set(), Normal)
+  val mrSatan = Guerrero(Humano(), 1000, List(), Set(), Normal)
+  val goku = Guerrero(Saiyajin(true, 3), 20000, List(), Set(), Normal)
+  val androide18 = Guerrero(Androide(1000), 0, List(), Set(), Normal)
   
   "mrSatan" should "cargarKi y subir 100 de ki por ser Guerrero" in {
 
@@ -27,7 +31,7 @@ class TestMovimientos extends FlatSpec with Matchers {
   
   "mrSatan" should "no hacer nada  cuando useItem Romo ya que no lo tiene en su inventario" in {
     
-    val humanito = Guerrero(Humano(), 1, List(), List(), Normal)
+    val humanito = Guerrero(Humano(), 1, List(), Set(), Normal)
     val luchadoresLuegoDeUsarItemRoma = usarItem(Roma()) ((mrSatan, Option(humanito)))
     
     assert(luchadoresLuegoDeUsarItemRoma._1 === mrSatan && luchadoresLuegoDeUsarItemRoma._2 === Option(humanito))
@@ -42,7 +46,7 @@ class TestMovimientos extends FlatSpec with Matchers {
   
   "bulma" should "quedar iconsciente al recibir ataque con item Romo y tener menos de 300 de ki" in {
     
-    val humanoConItemRomo = Guerrero(Humano(), 1000, List(Roma()), List(), Normal)
+    val humanoConItemRomo = Guerrero(Humano(), 1000, List(Roma()), Set(), Normal)
     val luchadoresLuegoDeUsarItemRoma = usarItem(Roma()) ((humanoConItemRomo, Option(bulma)))
     
     assert(luchadoresLuegoDeUsarItemRoma._2.get.estado === Inconsciente)
@@ -50,7 +54,7 @@ class TestMovimientos extends FlatSpec with Matchers {
   
    "bulma" should "quedar nomal al recibir ataque con item Romo de alguien que no posee dicho item aunque tiene menos de 300 de ki" in {
     
-    val humanoSinItemRomo = Guerrero(Humano(), 1000, List(), List(), Normal)
+    val humanoSinItemRomo = Guerrero(Humano(), 1000, List(), Set(), Normal)
     val luchadoresLuegoDeUsarItemRoma = usarItem(Roma()) ((humanoSinItemRomo, Option(bulma)))
     
     assert(luchadoresLuegoDeUsarItemRoma._2.get.estado === Normal)
