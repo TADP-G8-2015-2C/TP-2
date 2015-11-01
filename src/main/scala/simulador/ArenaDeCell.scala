53fdc630a9aed90b52bbbdec534dd8d87e3324fe
JuanchiRios <juanchirios04@hotmail.com>
2015-11-01 04:00:22 -0300

    refactor de los test por modificar Guerrero, modificacion de implementacion de roma, agregado sin test de: Arma de fuego, Arma Filosa, Semillas Del Hermintaneo, magia y plantee la estructura de fusion

--- src/main/scala/simulador/ArenaDeCell.scala
+++ src/main/scala/simulador/ArenaDeCell.scala
@@ -4,95 +4,99 @@ import scala.collection.immutable.List
 import scala.collection.immutable.Set
 import scala.collection.GenTraversableOnce
 
-
 object ArenaDeCell {
-  
-  case class Guerrero(raza: Raza, ki: Int = 0, items: List[Item] = List()
-      , movimientos: Set[Movimiento] = Set(), estado: Estado) {
-    
+
+  case class Guerrero(raza: Raza, ki: Int = 0, kiMax: Int, items: List[Item] = List(), movimientos: Set[Movimiento] = Set(), estado: Estado) {
+
     def aumentarKi(cuanto: Int) = copy(ki = ki + cuanto)
     def disminuirKi(cuanto: Int) = copy(ki = ki - cuanto)
-    
-    def aprenderMovimiento(unMovimiento: Movimiento) = copy(movimientos = movimientos +(unMovimiento))
-    
+    def aprenderMovimiento(unMovimiento: Movimiento) = copy(movimientos = movimientos + (unMovimiento))
     def agregarItem(unItem: Item) = copy(items = items.+:(unItem))
     def poseeItem(unItem: Item) = items.contains(unItem)
-    def removerItem(unItem: Item) = copy(items = items.filterNot { item => item == unItem })//este metodo lo hace, el que lo llama tiene que ser consciente de que tenga el item
+    def removerItem(unItem: Item) = copy(items = items.filterNot { item => item == unItem }) //este metodo lo hace, el que lo llama tiene que ser consciente de que tenga el item
+
+    def recuperarMaxPotencial() = copy(ki = kiMax)
+
+    def esbueno() = List(Saiyajin(_,_),Namekusein(),Humano()).contains(this.raza)//Ez game
     
-    def quedateInconsciente() = copy (estado = Inconsciente)
+    def disminuirMunicion(cant: Int) = {
+      this removerItem (Fuego(cant))
+      this agregarItem (Fuego(cant - 1))
+    }
+
+    def quedateInconsciente() = copy(estado = Inconsciente)
+
+    def cortarCola() = this //para que tipe
   }
-  
-  
+
   trait Estado {}
-  
+
   case object Inconsciente extends Estado
   case object Muerto extends Estado
   case object Normal extends Estado
-  
-  abstract class Item {
-    def aplicateSobre(luchadores: Luchadores): Luchadores
-  }
-  
-  case class Roma() extends Item{
-    def aplicateSobre(luchadores: Luchadores): Luchadores = {
-      val defensor = luchadores._2
-      val atacante = luchadores._1
-      
-      if(defensor.get.ki < 300 || defensor.get.raza != Androide()){
-          (atacante, Option(defensor.get.quedateInconsciente()))
+
+  trait Item {}
+  //esta parte quedo demasiado funcional pn, habría que delegar un poco y ver que se puede ahcer para no repetir tanta logica
+  case class UsarItem(item: Item) extends Movimiento {
+    def apply(luchadores: Luchadores) = {
+      if (!luchadores._1.poseeItem(item)) {
+        luchadores
+      } else {
+        (item, luchadores._1.raza, luchadores._2.get.raza) match {
+          case (Roma(), _, _) if luchadores._2.get.ki < 300 => (luchadores._1, luchadores._2.map(defe => defe quedateInconsciente))
+          case (Filosa(), _, Saiyajin(true, _)) => (luchadores._1, luchadores._2.map(defe => defe cortarCola))
+          case (Filosa(), _, _) => (luchadores._1, luchadores._2.map(defe => defe disminuirKi (luchadores._1.ki / 100)))
+          case (Fuego(cant), _, Humano()) if cant > 0 => (luchadores._1.disminuirMunicion(cant), luchadores._2.map(defe => defe disminuirKi (20)))
+          case (Fuego(cant), _, Namekusein()) if cant > 0 && luchadores._2.get.estado == Inconsciente => (luchadores._1.disminuirMunicion(cant), luchadores._2.map(defe => defe disminuirKi (10)))
+          case (Filosa(), _, Saiyajin(true, _)) => (luchadores._1, luchadores._2.map(defe => defe cortarCola))
+          case (SemillaDelErmitaño(), _, _) => (luchadores._1.recuperarMaxPotencial(), luchadores._2)
+        }
       }
-      else {
-        (atacante, defensor)
-      }
-      
     }
   }
-  
-  /*
-  case class Filosa() extends Item{
-    def aplicateSobre(luchadores: Luchadores) = {luchadores}
+
+  case class Roma() extends Item {}
+  case class Filosa() extends Item {}
+  case class Fuego(cant: Int) extends Item {}
+  case class SemillaDelErmitaño() extends Item {}
+
+  case class Magia(criterio: Luchadores => Luchadores) extends Movimiento {
+    def apply(luchadores: Luchadores) = criterio(luchadores)
   }
-  case class Fuego() extends Item{
-    def aplicateSobre(luchadores: Luchadores) = {luchadores}
-  }*/
-  
-  
+
+  case class Fusion(Compañero: Guerrero) extends Movimiento {
+    def apply(luchadores: Luchadores) = {
+      (luchadores._1) match {
+        case (luchador) if luchador.esbueno()=> luchadores//TODO insertar fusion
+        case _ => luchadores
+      }
+    }
+  }
+
   abstract class Raza
-  
+
   case class Monstruo() extends Raza {}
   case class Humano() extends Raza {}
-  case class Androide(bateria: Int = 0) extends Raza {}
+  case class Androide(bateria: Int = 0) extends Raza {
+    def quedateInconsciente() = this
+  }
   case class Namekusein() extends Raza {}
-  case class Saiyajin(cola: Boolean, nivel: Int = 0) extends Raza {}
+  case class Saiyajin(cola: Boolean, nivel: Int = 0) extends Raza {
+    def cortarCola() = this //Va a ser implementado mas adelante
+  }
 
   type Movimiento = Luchadores => Luchadores
-  
+
   type Luchadores = (Guerrero, Option[Guerrero])
-  
-  
-  val dejarseFajar = (luchadores: Luchadores) => luchadores//TODO posiblemente cambie cuando quiera saber los turnos
-  
+
+  val dejarseFajar = (luchadores: Luchadores) => luchadores //TODO posiblemente cambie cuando quiera saber los turnos
+
   val cargarKi = (luchadores: Luchadores) => {
     luchadores._1.raza match {
-      case Androide(_) => luchadores
-      case Saiyajin(_, nivel) => (luchadores._1.aumentarKi(150 * nivel), luchadores._2)//corregir y hacer que matchee con ss
-      case _ => (luchadores._1.aumentarKi(100), luchadores._2)
+      case Androide(_)                     => luchadores
+      case Saiyajin(_, nivel) if nivel > 0 => (luchadores._1.aumentarKi(150 * nivel), luchadores._2) //corregir y hacer que matchee con ss
+      case _                               => (luchadores._1.aumentarKi(100), luchadores._2)
     }
   }
-  
-  case class usarItem(itemAUsar: Item) extends Movimiento {
-    def apply(luchadores: Luchadores) = {
-      val usador = luchadores._1
-      val defendor = luchadores._2
-      
-      if(!usador.poseeItem(itemAUsar)){
-        luchadores
-      }
-      else {
-        itemAUsar.aplicateSobre(luchadores)
-      }
-    }
-    
-  }
-  
+
 }
---
+++ src/test/scala/simuladorTest/MovimientosSpec.scala
@@ -0,0 +1,65 @@
+
+import simulador.ArenaDeCell._
+import org.scalatest._
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import scala.collection.immutable.List
+import scala.collection.immutable.Set
+import scala.collection.GenTraversableOnce
+
+class MovimientosSpec extends FlatSpec with Matchers {
+
+  val bulma = Guerrero(Humano(), 100,50000 ,List(), Set(), Normal)
+  val mrSatan = Guerrero(Humano(), 1000, 50000, List(), Set(), Normal)
+  val goku = Guerrero(Saiyajin(true, 3), 20000, 50000, List(), Set(), Normal)
+  val androide18 = Guerrero(Androide(1000), 0, 50000, List(), Set(), Normal)
+
+  "mrSatan" should "cargarKi y subir 100 de ki por ser Guerrero" in {
+
+    assertResult(1100) {
+      cargarKi(mrSatan, None)._1.ki
+    }
+  }
+
+  "goku" should "cargarKi y subir ki en  150 * nivel de SS por ser Saiyajin" in {
+
+    assert(cargarKi(goku, None)._1.ki === 20450)
+  }
+
+  "androide18" should "cargarKi y no subir nada (seguir en 0) por ser un Androide" in {
+
+    assert(cargarKi(androide18, None)._1.ki === 0)
+  }
+
+  "mrSatan" should "no hacer nada  cuando useItem Romo ya que no lo tiene en su inventario" in {
+
+    val humanito = Guerrero(Humano(), 1, 50000, List(), Set(), Normal)
+    val luchadoresLuegoDeUsarItemRoma = UsarItem(Roma())((mrSatan, Option(humanito)))
+
+    assert(luchadoresLuegoDeUsarItemRoma._1 === mrSatan && luchadoresLuegoDeUsarItemRoma._2 === Option(humanito))
+  }
+
+  "androide18" should "queda igual porque los androides no se modifican al recibir ataque de item Romo" in {
+
+    val luchadoresLuegoDeUsarItemRoma = UsarItem(Roma())((mrSatan, Option(androide18)))
+
+    assert(luchadoresLuegoDeUsarItemRoma._1 === mrSatan && luchadoresLuegoDeUsarItemRoma._2 === Option(androide18))
+  }
+
+  "bulma" should "quedar iconsciente al recibir ataque con item Romo y tener menos de 300 de ki" in {
+
+    val humanoConItemRomo = Guerrero(Humano(), 1000, 50000, List(Roma()), Set(), Normal)
+    val luchadoresLuegoDeUsarItemRoma = UsarItem(Roma())((humanoConItemRomo, Option(bulma)))
+
+    assert(luchadoresLuegoDeUsarItemRoma._2.get.estado === Inconsciente)
+  }
+
+  "bulma" should "quedar nomal al recibir ataque con item Romo de alguien que no posee dicho item aunque tiene menos de 300 de ki" in {
+
+    val humanoSinItemRomo = Guerrero(Humano(), 1000, 50000, List(), Set(), Normal)
+    val luchadoresLuegoDeUsarItemRoma = UsarItem(Roma())((humanoSinItemRomo, Option(bulma)))
+
+    assert(luchadoresLuegoDeUsarItemRoma._2.get.estado === Normal)
+  }
+
+}
