0290defe607d11320ff75e5d7c47aa011b26b534
JuanchiRios <juanchirios04@hotmail.com>
2015-10-31 21:50:32 -0300

    metodos basicos de warrior

--- src/main/scala/warrior/Warrior.scala
+++ src/main/scala/warrior/Warrior.scala
@@ -3,12 +3,15 @@ package warrior
 import scala.collection.immutable.List
 import item.Item
 import movement.Movement
+import scala.collection.GenTraversableOnce
 
 case class Warrior(race: Race, ki: Int = 0, items: List[Item] = List(), movements: List[Movement] = List()) {
 
   def increaseKi(quantity: Int) = copy(ki = ki + quantity)
   def decreaseKi(quantity: Int) = copy(ki = ki - quantity)
-//  def learnMovement(movement: Movement) = copy(movements = movements ++ movement)
+  def learnMovement(aMovement: Movement) = copy(movements = movements.+:(aMovement))
+  def addItem(aItem: Item) = copy(items = items.+:(aItem))
+  def removeItem(aItem: Item) = copy(items = items.filterNot { item => item == aItem })//este metodo lo hace, el que lo llama tiene que ser consciente de que tenga el item
 
   def mostEffectiveMovementAgainst(opponent: Warrior) = "Should be implemented"
   def roundFight(aMovement: Movement, opponent: Warrior) = "Should be implemented"
