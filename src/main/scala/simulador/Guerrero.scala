97469528067fe2cd375e4f0c82be7a304786d270
JuanchiRios <juanchirios04@hotmail.com>
2015-11-04 14:23:31 -0300

    agrego criterioDeCombate ventaja de ki

--- src/main/scala/simulador/Guerrero.scala
+++ src/main/scala/simulador/Guerrero.scala
@@ -75,6 +75,11 @@ case class Guerrero(raza: Raza, ki: Int = 0, kiMax: Int, items: List[Item] = Lis
        throw new RuntimeException("No tiene movimiento mas efectivo contra oponente")
      else movMasEfectivo
   }
+  def mayorVentajaKi(luchadores: Luchadores): Int = {
+    luchadores._1.ki - luchadores._2.ki
+  }
+  
+
 }
 
 trait Estado {}
