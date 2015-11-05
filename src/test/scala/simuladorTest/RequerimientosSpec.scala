08068ea2356cfb994407c934bf55a3d788330119
JuanchiRios <juanchirios04@hotmail.com>
2015-11-05 19:58:21 -0300

    agrego file para tests de requerimientos

---
+++ src/test/scala/simuladorTest/RequerimientosSpec.scala
@@ -0,0 +1,23 @@
+package simuladorTest
+
+  import simulador._
+import simulador.ArenaDeCell._
+import simulador.Guerrero._
+
+import org.scalatest._
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+import scala.collection.immutable.List
+import scala.collection.immutable.Set
+import scala.collection.GenTraversableOnce
+
+import simulador.TuplasUtils._
+import simuladorTest.SetUp._
+
+
+class RequerimientosSpec extends FlatSpec with Matchers {
+//goku.movimentoMasEfectivoContra(vegeta)(unCriterio)
+
+  
+}
