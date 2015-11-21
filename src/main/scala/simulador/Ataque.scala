df06249129f514d87211b4697c8da0f9eba18190
JuanchiRios <juanchirios04@hotmail.com>
2015-11-20 21:19:14 -0300

    ataques energicos

--- src/main/scala/simulador/Ataque.scala
+++ src/main/scala/simulador/Ataque.scala
@@ -32,49 +32,53 @@ abstract class AtaqueEnergia(ataque: Function[Luchadores, (Int,Int)])
 
 //case class Fisico(ataque: Movimiento) extends Ataque {}
 
-case object muchosGolpesNinja extends Movimiento ((luchadores: Luchadores) => {
-    (luchadores._1.raza, luchadores._2.raza) match {
-      case (Humano, Androide) => (luchadores._1.disminuirKi(10), luchadores._2)
-      case (_, _) => if ((luchadores._1.ki > luchadores._2.ki)) {
-        (luchadores._1, luchadores._2 disminuirKi(20))
-      } else {
-        (luchadores._1.disminuirKi(20), luchadores._2)
-      }
+case object muchosGolpesNinja extends Movimiento((luchadores: Luchadores) => {
+  (luchadores._1.raza, luchadores._2.raza) match {
+    case (Humano, Androide) => (luchadores._1.disminuirKi(10), luchadores._2)
+    case (_, _) => if ((luchadores._1.ki > luchadores._2.ki)) {
+      (luchadores._1, luchadores._2 disminuirKi (20))
+    } else {
+      (luchadores._1.disminuirKi(20), luchadores._2)
     }
-})
-
-case object explotar extends Movimiento ((luchadores: Luchadores) => {
-    (luchadores._1.raza, luchadores._2.raza) match {
-      case (Monstruo, Namekusein)        => (luchadores._1.morite(), luchadores._2 disminuirKiNamekusein (luchadores._1.ki * 2))
-      case (Androide, Namekusein) => (luchadores._1.morite(), luchadores._2 disminuirKiNamekusein (luchadores._1.ki * 3))
-      case (Monstruo, _)                   => (luchadores._1.morite(), luchadores._2 disminuirKi (luchadores._1.ki * 2) )
-      case (Androide, _)            => (luchadores._1.morite(), luchadores._2 disminuirKi (luchadores._1.ki * 3))
-      case (_, _)                            => luchadores
-    }
-})
-
-case class onda(kiNecesario: Int) extends Movimiento ((luchadores: Luchadores) => {
-    (luchadores._1.raza, luchadores._2.raza) match {   
-      case (_, Monstruo) if luchadores._1.ki >= kiNecesario => (luchadores._1.disminuirKi(kiNecesario), luchadores._2.disminuirKi(kiNecesario / 2) )
-      case (_, Androide) if luchadores._1.ki >= kiNecesario => (luchadores._1.disminuirKi(kiNecesario), luchadores._2.aumentarKi(kiNecesario) )
-      case (_, _) if luchadores._1.ki >= kiNecesario => (luchadores._1.disminuirKi(kiNecesario), luchadores._2.disminuirKi(kiNecesario * 2) )
-      case (_, _) => luchadores
-    }
- //ES MUY FEO ESTO MUCHISIMA LOGICA REPETIDA MATAN CONEJITOS 
-})
-case object genkidama extends Movimiento((luchadores: Luchadores) => {
-  (luchadores._1.estado,luchadores._2.raza) match {
-    case (NiUnaMenos(rounds),_) =>(luchadores._1, luchadores._2.disminuirKi(math.pow(10, rounds).asInstanceOf[Int]))
-    case _ => luchadores
   }
 })
 
-  /*   case class atacarCon(ataque: Ataque) extends Movimiento {
-      def apply(luchadores: Luchadores) = {
-        (ataque,luchadores._1,luchadores._2.get) match{
-          case (Energia)
-          
-        }
-      }
-    }*/
+case object explotar extends Movimiento((luchadores: Luchadores) => {
+  (luchadores._1.raza, luchadores._2.raza) match {
+    case (Monstruo, Namekusein) => (luchadores._1.morite(), luchadores._2 disminuirKiNamekusein (luchadores._1.ki * 2))
+    case (Androide, Namekusein) => (luchadores._1.morite(), luchadores._2 disminuirKiNamekusein (luchadores._1.ki * 3))
+    case (Monstruo, _)          => (luchadores._1.morite(), luchadores._2 disminuirKi (luchadores._1.ki * 2))
+    case (Androide, _)          => (luchadores._1.morite(), luchadores._2 disminuirKi (luchadores._1.ki * 3))
+    case (_, _)                 => luchadores
+  }
+})
+
+abstract class AtaqueEnergico(unAtaque: Luchadores =>(Int,Int)) extends Movimiento((luchadores: Luchadores) => {
+   val (ki1,ki2) = unAtaque(luchadores)
+  (luchadores._1.raza, luchadores._2.raza) match {
+      case (_, Androide)  => (luchadores._1.disminuirKi(ki1), luchadores._2.aumentarKi(ki2))
+      case (_, _)         => (luchadores._1.disminuirKi(ki1), luchadores._2.disminuirKi(ki2))
+    }
+})
+
+case class onda(kiNecesario: Int) extends AtaqueEnergico((luchadores: Luchadores) => {
+  val (l1,l2) = luchadores
+  if (l1.ki >= kiNecesario)
+      (l1.raza, l2.raza) match {
+      case (_, Monstruo) => (kiNecesario, kiNecesario / 2)
+      case (_, Androide) => (kiNecesario,kiNecesario)
+      case (_, _) => (kiNecesario, kiNecesario * 2)
+    }
+  else
+    (0,0)
+})
+
+case object genkidama extends AtaqueEnergico((luchadores: Luchadores) => {
+  val (l1,l2) = luchadores
+  (l1.estado, l2.raza) match {
+    case (NiUnaMenos(rounds), _) => (0, math.pow(10, rounds).asInstanceOf[Int])
+    case _                       => (0,0)
+  }
+})
+
 
